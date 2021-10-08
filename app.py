08c75fde5230a68a679c809718475d51545709ca
Viviana Urrego <viliurr@gmail.com>
2021-10-08 12:48:06 -0500

    Lo que llevo hasta el momento

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from os import name
-from flask import Flask, request, render_template, redirect,url_for
+from flask import Flask, request, render_template, redirect,url_for,session,flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import query
 
@@ -12,26 +12,23 @@ app.secret_key = 'some-secret-key'
 db = SQLAlchemy(app)
 
 # Importar los modelos
-from models import Admin, User, Profile
+from models import Admin, User, Profile, Type_food
 
 # Crear el esquema de la DB
 db.create_all()
 db.session.commit()
 
-#Rutas de páginas
-
+#Rutas de página de inicio
 @app.route('/')
 def get_home():
-    
  return render_template("home.html")
-
 
 # Ruta del registro de usuario
 @app.route('/register')
 def register():
     return render_template("register.html")
 
-
+# Ruta para crear el registro de usuario
 @app.route('/create_user', methods=['POST'])
 def create_user():
 
@@ -42,83 +39,129 @@ def create_user():
         print(email)
         print(password)
 
-        user = User(name, email,password)
+        user = User(name,email,password)
         db.session.add(user)
         db.session.commit()
+
+        found=User.query.filter(User.email==email)
+        session['user_id'] = found[0].id
+        print (session['user_id'])
+
+        return render_template("profile.html")
+
+# ruta para validar el usuario
+@app.route('/check_user', methods=['POST'])
+def check_user():
+    email = request.form["email"]
+    password = request.form["password"]
+
+    if (email == "correo_Admin@gmail.com" and password =="admin"):
+        return redirect (url_for('admin'))
+
+    else:
+        users=User.query.filter(User.password==password,User.email==email)
+
+        try:
+            if (users[0] is not None):
+                session['user_id'] = users[0].id 
+                print(session['user_id'] ) 
+                return render_template("index.html") 
+                
+        except:
+            print ("Email o Password incorrectos")
+            flash("Email o Password incorrectos","alert-warning")
+            return render_template('login.html')
+
+# Ruta para el perfil de usuario
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+# ruta para crear el perfil del usuario
+@app.route('/create_profile', methods=['POST'])
+def create_profile():
+
+        sex = request.form["sex"]
+        age = request.form["age"]
+        height = request.form["height"]
+        weight = request.form["weight"]
+        insulin_type = request.form["insulin_type"]
+        user_id = session['user_id']
+        
+        profile =Profile(sex, age, height, weight, insulin_type,user_id)
+        
+        db.session.add(profile)
+        db.session.commit()   
+        session.pop('user_id', None)
         return render_template("login.html")
  
 # Ruta para ingresar el usuario
 @app.route('/login')
 def login():
   return render_template("login.html")
-
-@app.route('/check_user', methods=['POST'])
-def check_user():
-    email = request.form["email"]
-    password = request.form["password"]
-    users=User.query.filter(User.password==password,User.email==email)
-
-    try:
-        if(users[0] is not None):
-            return render_template("index.html")
-
-    except:
-       return render_template("login.html")
+  
+# ruta para el deslogueo del usuario
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return render_template("login.html")
 
 # Ruta iniciada la sesion
-@app.route('/index',methods=['POST'])
+@app.route('/index')
 def index():
   return render_template("index.html")
 
+# ruta para visualizar el perfil del usuario
+@app.route('/view_profile')
+def view_profile():
+     return render_template("view_profile.html")
 
-# Ruta para el perfil de usuario
-@app.route('/profile',methods=['GET','POST'])
-def profile():
+
+@app.route('/show_profile')    
+def show_profile():
+    consulta = Profile.query.all(Profile.user_id==session['user_id'])
+    datos_perfil=Profile.query.all()
+    
+    print(session['user_id'])
+    print(consulta.sex)
+    print(datos_perfil)
+
+    return "hola"
+   
+# Ruta para el calculo de insulina
+@app.route('/Insulin_calculation')
+def Insulin_calculation():
+    return render_template("Insulin_calculation.html")
+
+# ruta para alimentos
+@app.route('/type_food')
+def type_food():
+  return render_template("type_food.html")
+
+@app.route('/add_food',methods=['GET','POST'])
+def add_food():
     name = request.form["name"]
-    email = request.form["email"]
-    password = request.form["password"]
-    print(name)
-    print(email)
-    print(password)
+    category = request.form["category"] 
+    quantity = request.form["quantity"]
+    carbohydrate= request.form["carbohydrate"]
+    admin_id = session['Admin.id']
 
-    user = User(name, email,password)
-    db.session.add(user)
-    db.session.commit()
-    return redirect(url_for("/profile",user=user.id))
+    type_food =Type_food( name,category,quantity,carbohydrate,admin_id )
+    db.session.add(type_food)
+    db.session.commit()  
+    session.pop('Admin.id', None)
+    return "Su registro se creo con exito"
 
-@app.route('/create_profile/<user>')
-def create_profile(user):
-    sex = request.form["sex"]
-    age = request.form["age"]
-    height = request.form["heigth"]
-    weight = request.form["weight"]
-    insulin_type = request.form["insulin_type"]
-    user_id = request.form["user_id"]
+#Rutas de ingreso administrador
+@app.route('/admin')
+def admin():
+  return render_template("index_admin.html")
 
-    print(sex)
-    print(age)
-    print(height)
-    print(weight)
-    print(insulin_type)
-    #print(user_id)
-
-    profile = Profile(sex, age, height, weight, insulin_type)
-    db.session.add(profile)
-    db.session.commit()
-    print(db.session)
-    print(profile.id)
-    return "SU perfil se registro exitosamente"
-
-
-#Rutas de otras acciones
-@app.route('/admin', methods=['GET','POST'])
+@app.route('/crud_admin', methods=['GET','POST'])
 def crud_admin():
     if request.method == 'GET':
-        # Hago algo
-        print("Llego un GET")
-        # Insertar un admin
-        email = "admin2@cod.com"
-        password = "54321"
+        email = "correo_Admin@gmail.com"
+        password = "admin"
         entry = Admin(email, password)
         db.session.add(entry)
         db.session.commit()
@@ -133,7 +176,6 @@ def crud_admin():
         print("Contraseña:" + password)
 
         # Insertar en la base de datos la canción
-
         return 'Se registro el admin exitosamente'
 
 @app.route('/updateadmin',methods=['GET','POST'])
